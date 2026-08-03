8a6fe8011dc2c0dfc99cae054e1d61874ca84309
mubharak2005 <151344058+mubharak2005@users.noreply.github.com>
2026-08-03 16:53:49 +0700

    Create check_hand_body_price.php

---
+++ scripts/check_hand_body_price.php
@@ -0,0 +1,8 @@
+<?php
+$db = new SQLite3(__DIR__ . '/../database/database.sqlite');
+$product = $db->querySingle("SELECT id, slug, name, price FROM products WHERE slug = 'ryoki-hand-body'", true);
+if ($product) {
+    echo $product['id'] . '|' . $product['slug'] . '|' . $product['name'] . '|' . $product['price'] . PHP_EOL;
+} else {
+    echo "not-found" . PHP_EOL;
+}
